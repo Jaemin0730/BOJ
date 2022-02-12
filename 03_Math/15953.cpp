2aeb6a06b316befa33768a90ad9bf827b28184f6
Jaemin0730 <jaemkim01@hanyang.ac.kr>
2022-02-12 15:05:11 +0900

    2022.02.11 푼 수학 문제

---
+++ 03_Math/15953.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+}
