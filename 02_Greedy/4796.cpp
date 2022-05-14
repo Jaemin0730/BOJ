5e691dc8583ebfb4a7b2fe8e12560b4fe4e9ef39
Jaemin0730 <jaemkim01@hanyang.ac.kr>
2022-05-14 23:51:36 +0900

    2022.05.14 푼 그리디 문제

---
+++ 02_Greedy/4796.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+#define pa pair<int, int>
+
+int main(void)
+{
+    u;
+    int n = 1;
+    while (1) {
+        int P, L, V;
+        cin >> L >> P >> V;
+        if (L == 0 && P == 0 && V == 0) {
+            break;
+        }
+        int sol = V - (V / P) * P;
+        //cout << sol << "!!\n";
+        sol += L * (V / P);
+        //cout << sol << "??\n";
+        cout << "Case " << n << ": " << sol << "\n";
+        n++;
+    }
+}
