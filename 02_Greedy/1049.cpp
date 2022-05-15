fad191eea924950ad860fdeda70826014fe93ca1
Jaemin0730 <jaemkim01@hanyang.ac.kr>
2022-05-15 23:27:33 +0900

    2022.05.15 푼 그리디 문제

--- 02_Greedy/1049.cpp
+++ 02_Greedy/1049.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 #define u ios::sync_with_stdio(false); cin.tie(NULL)
 #define ll long long
-#define p pair<int, int>
 
 int main(void)
 {
     u;
-    
+    int n, m, sol, p, q, X, Y;
+    cin >> n >> m;
+    int set = 1e9, alo = 1e9;
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+        set = min(set, a);
+        alo = min(alo, b);
+    }
+    p = n / 6;
+    q = n % 6;
+    sol = min(p * set + q * alo, min(set * (p + 1), alo * n));
+    cout << sol;
 }
