036e9d1735c755129ba08dde3e15dde77d48c867
Jaemin0730 <jaemkim01@hanyang.ac.kr>
2022-05-15 22:24:09 +0900

    2022.05.15 푼 그리디 문제

---
+++ 02_Greedy/1049.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+#define p pair<int, int>
+
+int main(void)
+{
+    u;
+    
+}
